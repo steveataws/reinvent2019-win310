c317551ec42215c7893f6e582fc3cc7c84b3e63a
Nicki Stone <nickik@amazon.com>
2019-11-16 18:59:16 -0800

    more changes to attempt to get it to work

--- demos/containers/src/Containers/ContainersStack.cs
+++ demos/containers/src/Containers/ContainersStack.cs
@@ -2,6 +2,8 @@ using Amazon.CDK;
 using Amazon.CDK.AWS.EC2;
 using Amazon.CDK.AWS.ECS;
 using Amazon.CDK.AWS.ECS.Patterns;
+using System.IO;
+using System;
 
 namespace Containers
 {
@@ -15,20 +17,25 @@ namespace Containers
             clusterProps.Vpc = vpc;
             Cluster cluster = new Cluster(this, "Cluster", clusterProps);
             FargateTaskDefinition taskDef = new FargateTaskDefinition(this, "FargateTaskDefinition");
-            ContainerDefinitionOptions containerOptions = new ContainerDefinitionOptions() {
-                Image = ContainerImage.FromAsset("app/")
-            };
-
-            taskDef.AddContainer("Container", containerOptions).AddPortMappings(new PortMapping() {
+         
+            ContainerDefinitionOptions containerOptions = new ContainerDefinitionOptions();
+            var rootDirectory = Directory.GetCurrentDirectory();
+       
+            var path = Path.GetFullPath(Path.Combine(rootDirectory, @"dotnetapp/"));
+            //var image = ContainerImage.FromAsset(path);
+            var image = ContainerImage.FromAsset("dotnetapp");
+            containerOptions.Image = image;
+            var portMapping = new PortMapping() {
                 ContainerPort = 80,
                 HostPort = 80
-            });
-
-            ApplicationLoadBalancedFargateService service = new ApplicationLoadBalancedFargateService(this, "DotnetFargateApp", new ApplicationLoadBalancedFargateServiceProps() {
+            };
+            taskDef.AddContainer("Container", containerOptions).AddPortMappings(portMapping);
+            var serviceProps = new ApplicationLoadBalancedFargateServiceProps() {
                 MemoryLimitMiB = 512,
                 Cpu = 256,
                 TaskDefinition = taskDef
-            });
+            };
+            ApplicationLoadBalancedFargateService service = new ApplicationLoadBalancedFargateService(this, "DotnetFargateApp", serviceProps);
             
         }
     }
