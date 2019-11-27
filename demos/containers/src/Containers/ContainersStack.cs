ab3dc29c81becc56cc7a16869ac7ab7d93d115d2
Steve Roberts <strobe@amazon.com>
2019-11-26 21:12:50 -0800

    Update to GA libraries

--- demos/containers/src/Containers/ContainersStack.cs
+++ demos/containers/src/Containers/ContainersStack.cs
@@ -1,9 +1,9 @@
+using System.IO;
+
 using Amazon.CDK;
 using Amazon.CDK.AWS.EC2;
 using Amazon.CDK.AWS.ECS;
 using Amazon.CDK.AWS.ECS.Patterns;
-using System.IO;
-using System;
 
 namespace Containers
 {
@@ -12,31 +12,39 @@ namespace Containers
         public ContainersStack(Construct parent, string id, IStackProps props) : base(parent, id, props)
         {
             // The code that defines your stack goes here
-            Vpc vpc = new Vpc(this, "VPC");
-            ClusterProps clusterProps = new ClusterProps();
-            clusterProps.Vpc = vpc;
-            Cluster cluster = new Cluster(this, "Cluster", clusterProps);
-            FargateTaskDefinition taskDef = new FargateTaskDefinition(this, "FargateTaskDefinition");
-         
-            ContainerDefinitionOptions containerOptions = new ContainerDefinitionOptions();
+            var vpc = new Vpc(this, "VPC");
+
+            var cluster = new Cluster(this, "Cluster", new ClusterProps
+            {
+                Vpc = vpc
+            });
+
+            var taskDef = new FargateTaskDefinition(this, "FargateTaskDefinition");
+
             var rootDirectory = Directory.GetCurrentDirectory();
-       
             var path = Path.GetFullPath(Path.Combine(rootDirectory, @"dotnetapp/"));
-            //var image = ContainerImage.FromAsset(path);
-            var image = ContainerImage.FromAsset("dotnetapp");
-            containerOptions.Image = image;
-            var portMapping = new PortMapping() {
+
+            var containerOptions = new ContainerDefinitionOptions
+            {
+                Image = ContainerImage.FromAsset("dotnetapp")
+            };
+
+            var portMapping = new PortMapping()
+            {
                 ContainerPort = 80,
                 HostPort = 80
             };
+
             taskDef.AddContainer("Container", containerOptions).AddPortMappings(portMapping);
-            var serviceProps = new ApplicationLoadBalancedFargateServiceProps() {
+
+            var serviceProps = new ApplicationLoadBalancedFargateServiceProps()
+            {
                 MemoryLimitMiB = 512,
                 Cpu = 256,
                 TaskDefinition = taskDef
             };
+
             ApplicationLoadBalancedFargateService service = new ApplicationLoadBalancedFargateService(this, "DotnetFargateApp", serviceProps);
-            
         }
     }
 }
