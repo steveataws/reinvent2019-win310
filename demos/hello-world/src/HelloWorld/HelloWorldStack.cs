b85b07044483a4777df47cdafe845f440b5b2667
Steve Roberts <strobe@amazon.com>
2019-11-26 20:29:57 -0800

    Convert HelloWorld demo to deploy a static website to an Amazon S3 bucket

--- demos/hello-world/src/HelloWorld/HelloWorldStack.cs
+++ demos/hello-world/src/HelloWorld/HelloWorldStack.cs
@@ -1,5 +1,7 @@
 using Amazon.CDK;
 using Amazon.CDK.AWS.S3;
+using Amazon.CDK.AWS.S3.Deployment;
+using Amazon.CDK.AWS.IAM;
 
 namespace HelloWorld
 {
@@ -7,12 +9,39 @@ namespace HelloWorld
     {
         public HelloWorldStack(Construct parent, string id, IStackProps props) : base(parent, id, props)
         {
-            // The code that defines your stack goes here
-            //code to create a public s3 bucket
-            BucketProps bucketProps = new BucketProps();
-            bucketProps.AccessControl = BucketAccessControl.PUBLIC_READ_WRITE;
-            bucketProps.BucketName = "reinvent2019cdkdemo";
-            Bucket bucket = new Bucket(this, "ReinventBucket", bucketProps);
+            // Create a S3 bucket to host a static website, with global read access
+            var bucket = new Bucket(this, "ReinventBucket", new BucketProps
+            {
+                BucketName = "reinvent2019cdkdemo",
+                AccessControl = BucketAccessControl.PUBLIC_READ,
+                WebsiteIndexDocument = "index.html"
+            });
+
+            // grant access to the pages and resources in the website (setting
+            // Public Read Only on the bucket is not sufficient)
+            bucket.AddToResourcePolicy(new PolicyStatement(new PolicyStatementProps
+            {
+                Effect = Effect.ALLOW,
+                Actions = new [] { "s3:GetObject" },
+                Resources = new [] { bucket.ArnForObjects("*") },
+                Principals = new IPrincipal[]
+                {
+                    new AnyPrincipal()
+                }
+            }));
+
+            // deploy the site
+            new BucketDeployment(this, "ReinventBucketDeployment", new BucketDeploymentProps
+            {
+                DestinationBucket = bucket,
+                Sources = new [] { Source.Asset("./site-contents") }
+            });
+
+            // emit the url of the website for convenience
+            new CfnOutput(this, "BucketWebsiteUrl", new CfnOutputProps
+            {
+                Value = bucket.BucketWebsiteUrl
+            });
         }
     }
 }
