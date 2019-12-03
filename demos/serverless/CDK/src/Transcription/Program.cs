b44193d2bf136e9d1918afa686c424fc83c047c6
Steve Roberts <strobe@amazon.com>
2019-12-02 18:16:02 -0800

    Added serverless demo

---
+++ demos/serverless/CDK/src/Transcription/Program.cs
@@ -0,0 +1,14 @@
+﻿using Amazon.CDK;
+
+namespace Transcription
+{
+    sealed class Program
+    {
+        public static void Main(string[] args)
+        {
+            var app = new App();
+            new TranscriptionStack(app, "TranscriptionStack");
+            app.Synth();
+        }
+    }
+}
