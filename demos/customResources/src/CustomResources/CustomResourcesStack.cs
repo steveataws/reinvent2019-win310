dfbbd4b82456790ef71a0ad2fb161244d85310b0
Steve Roberts <strobe@amazon.com>
2019-12-03 08:50:09 -0800

    Remove acces to bucket

--- demos/customResources/src/CustomResources/CustomResourcesStack.cs
+++ demos/customResources/src/CustomResources/CustomResourcesStack.cs
@@ -9,10 +9,8 @@ namespace CustomResources
     {
         public CustomResourcesStack(Construct parent, string id, IStackProps props) : base(parent, id, props)
         {
-            // The code that defines your stack goes here
             var bucket = new Bucket(this, "ReinventBucket", new BucketProps
             {
-                AccessControl = BucketAccessControl.PUBLIC_READ_WRITE,
                 BucketName = "reinvent2019cdkdemo-customresources"
             });
 
